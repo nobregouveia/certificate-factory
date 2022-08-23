c3b3fd6e622a5288215a8474e2542919733fdce2
André Gouveia <nobregouveia@icloud.com>
2022-08-23 10:04:06 -0300

    Aletrações de logos

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -25,7 +25,7 @@
             <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
                 aria-hidden="true" id="iconSidenav"></i>
             <a class="navbar-brand m-0" href="#" target="_blank">
-                <img src="{{ asset('../assets/img/logo-gerdau-escudo-256.png') }}" class="navbar-brand-img h-100"
+                <img src="{{ asset('../assets/img/logo2.png') }}" class="navbar-brand-img h-100"
                     alt="main_logo">
                 <span class="ms-1 font-weight-bold text-white">Gerador de Certificado</span>
             </a>
